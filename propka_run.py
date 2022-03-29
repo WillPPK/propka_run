8aa4cf788edb9a2002a0ea30f3a86c48a429abfa
WillPPK <74375293+WillPPK@users.noreply.github.com>
2022-03-29 14:01:12 +0200

    Update propka_run.py

--- propka_run.py
+++ propka_run.py
@@ -1,16 +1,18 @@
+#write file with python propka_run.py xxx.pdb xxx.xtc
+
 import MDAnalysis as mda
 import sys
 from MDAnalysisTests.datafiles import PDB, XTC
-u = mda.Universe(sys.argv[1],sys.argv[2])
-import propkatraj
-protein = u.select_atoms('protein')
+u = mda.Universe(sys.argv[1],sys.argv[2]) #import file here
+import propkatraj #import module
+protein = u.select_atoms('protein') #defining your protein
 pkatraj = propkatraj.PropkaTraj(protein)
 pkatraj.run(verbose=True)
 pkatraj.pkas
 pkatraj.pkas.describe()
 data = pkatraj.pkas
 
-data.to_csv("pka-trajctory-full.csv")
+data.to_csv("pka-trajctory-full.csv") #save file
 
 data2 = pkatraj.pkas.describe()
 
